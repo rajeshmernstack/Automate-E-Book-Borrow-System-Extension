4df1725a4244ae1b1bb6b55d7596e93ac372b138
Rajesh Kumar <rajesh.bcsf18@iba-suk.edu.pk>
2022-10-16 23:21:36 +0500

    Fixed The Bug

--- background.js
+++ background.js
@@ -7,19 +7,22 @@ chrome.runtime.onInstalled.addListener(() => {
 
 console.log("I am background.js")
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && /^https/.test(tab.url)) {
-        chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            files: ["./content-script.js"]
-        })
-            .then(() => {
-                console.log("INJECTED THE FOREGROUND SCRIPT.");
-            })
-            .catch(err => console.log(err));
-    }
-});
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//     if (changeInfo.status === 'complete' && /^https/.test(tab.url)) {
+//         chrome.scripting.executeScript({
+//             target: { tabId: tabId },
+//             files: ["./content-script.js"]
+//         })
+//             .then(() => {
+//                 console.log("INJECTED THE FOREGROUND SCRIPT.");
+//             })
+//             .catch(err => console.log(err));
+//     }
+// });
 
+
+
+var myTab = 0;
 chrome.windows.getAll({ populate: true }, function (winData) {
     for (var i in winData) {
         var winTabs = winData[i].tabs;
@@ -29,16 +32,25 @@ chrome.windows.getAll({ populate: true }, function (winData) {
             // console.log(winTabs[j]);
             if (winTabs[j].url.includes("overdrive.com/media/")) {
                 console.log(winTabs[j])
+                if(myTab === 0 ){
+                    myTab = winTabs[j].id
+                }
+                    
             }
         }
     }
 });
 
-
 function fetchMatchedTabs() {
     allTabs.forEach(myTab => {
         if (myTab.url.includes("overdrive.com/media/")) {
             matchedTabs.push(myTab);
+            
         }
     });
 }
+
+chrome.tabs.sendMessage(myTab, {some: 'abc'}, function(r) {
+    console.log(myTab)
+    console.log(r)
+  });
--- content-script.js
+++ content-script.js
@@ -1,7 +1,18 @@
 
+
+console.log("OK")
+
 // function randomNumber(min, max) { 
 //   return Math.random() * (max - min) + min;
 // }
 
 // // Function call
 // document.write( randomNumber(1, 5) ); 
+
+
+chrome.runtime.onMessage.addListener(function(req, sender, cb) {
+    console.log(sender)
+    console.log(req);
+    cb({msg: 'back'});
+  }
+);
