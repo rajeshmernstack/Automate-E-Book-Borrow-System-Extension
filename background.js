43553b53c5059aa0ac38dbe16b0e357516bdac68
Rajesh Kumar <rajesh.bcsf18@iba-suk.edu.pk>
2022-10-17 17:03:55 +0500

    Done

--- background.js
+++ background.js
@@ -1,61 +1,44 @@
-chrome.runtime.onInstalled.addListener(() => {
-    // default state goes here
-    // this runs ONE TIME ONLY (unless the user reinstalls your extension)
-    console.log("I am onInstalled Function, I will be visible one time only.")
-});
 
 
+
+var workingTabs = [];
 console.log("I am background.js")
-
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//     if (changeInfo.status === 'complete' && /^https/.test(tab.url)) {
-//         chrome.scripting.executeScript({
-//             target: { tabId: tabId },
-//             files: ["./content-script.js"]
-//         })
-//             .then(() => {
-//                 console.log("INJECTED THE FOREGROUND SCRIPT.");
-//             })
-//             .catch(err => console.log(err));
-//     }
-// });
-
-
-
-var myTab = 0;
 chrome.windows.getAll({ populate: true }, function (winData) {
     for (var i in winData) {
         var winTabs = winData[i].tabs;
         var totTabs = winTabs.length;
         for (var j = 0; j < totTabs; j++) {
-            // allTabs.push(winTabs[j]);
-            // console.log(winTabs[j]);
             if (winTabs[j].url.includes("overdrive.com/media/")) {
                 console.log(winTabs[j])
-                if(myTab === 0 ){
-                    myTab = winTabs[j].id
-                }
-                    
+                workingTabs.push(winTabs[j]);
             }
         }
     }
 });
+var i = 0;
 
-function fetchMatchedTabs() {
-    allTabs.forEach(myTab => {
-        if (myTab.url.includes("overdrive.com/media/")) {
-            matchedTabs.push(myTab);
-            
-        }
-    });
-}
+var autoBorrow = setInterval(() => {
+    if (i < workingTabs.length) {
 
-// chrome.tabs.sendMessage(myTab, {some: 'abc'}, function(r) {
-//     console.log(myTab)
-//     console.log(r)
-//   });
-chrome.tabs.query({active: true, currentWindow: true},function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-        console.log(response);
-    });
-  });
+
+        chrome.scripting.executeScript({
+            target: { tabId: workingTabs[i].id },
+            func: () => {
+                // write your code here
+                if (document.querySelector('.TitleActionButton.u-allCaps.button.radius.is-button.secondary.is-borrow.js-borrow.secondary') != null) {
+                    console.log("Borrow Button Found")
+                } else {
+                    console.log("Borrow Button Not Found")
+                }
+            },
+        });
+
+        chrome.windows.update(workingTabs[i].windowId, { focused: true }, (window) => {
+            chrome.tabs.update(workingTabs[i].id, { active: true })
+            i++;
+        })
+    } else {
+        clearInterval(autoBorrow);
+        i = 0;
+    }
+}, 5000)
