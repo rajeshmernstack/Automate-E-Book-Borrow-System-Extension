4f42fb21a0c4134ef062b9ddf52736d707e100d1
Rajesh Kumar <rajesh.bcsf18@iba-suk.edu.pk>
2022-10-18 01:09:35 +0500

    Auto Borrow and Download Feature Done

--- background.js
+++ background.js
@@ -2,6 +2,7 @@
 
 
 var workingTabs = [];
+var downloadTab = {};
 console.log("I am background.js")
 chrome.windows.getAll({ populate: true }, function (winData) {
     for (var i in winData) {
@@ -12,6 +13,9 @@ chrome.windows.getAll({ populate: true }, function (winData) {
                 console.log(winTabs[j])
                 workingTabs.push(winTabs[j]);
             }
+            if (winTabs[j].url.includes("overdrive.com/account/loans")) {
+                downloadTab = winTabs[j]
+            }
         }
     }
 });
@@ -20,15 +24,33 @@ var i = 0;
 var autoBorrow = setInterval(() => {
     if (i < workingTabs.length) {
 
-
         chrome.scripting.executeScript({
             target: { tabId: workingTabs[i].id },
             func: () => {
                 // write your code here
                 if (document.querySelector('.TitleActionButton.u-allCaps.button.radius.is-button.secondary.is-borrow.js-borrow.secondary') != null) {
-                    console.log("Borrow Button Found")
+                    document.querySelector('.TitleActionButton.u-allCaps.button.radius.is-button.secondary.is-borrow.js-borrow.secondary').click();
+                    var borrowInner = setInterval(() => {
+
+                        if (document.querySelector('button.radius.secondary.contrast.u-allCaps.borrow-button') != null) {
+                            console.log("Borrow Button 2 Found")
+
+
+                            document.querySelector('button.radius.secondary.contrast.u-allCaps.borrow-button').click();
+                            clearInterval(borrowInner);
+                            setTimeout(() => {
+
+                                window.close();
+
+                            }, 4000)
+
+                        } else {
+                            console.log("Borrow Button 2 Not Found")
+                        }
+                    }, 5000)
+                    console.log("Brorrow Buttton 1 Found")
                 } else {
-                    console.log("Borrow Button Not Found")
+                    console.log("Borrow Button 1 Not Found")
                 }
             },
         });
@@ -39,6 +61,48 @@ var autoBorrow = setInterval(() => {
         })
     } else {
         clearInterval(autoBorrow);
-        i = 0;
+        console.log("Borrowing Done")
+        console.log(downloadTab)
+        chrome.tabs.reload(downloadTab.id);
+        setTimeout(() => {
+            chrome.scripting.executeScript({
+                target: { tabId: downloadTab.id },
+                func: () => {
+
+                    var downloadButtonCounter = 0;
+                    var allDownloadButtons = document.querySelectorAll('.loan-button-nonkindle.button.radius.primary.downloadButton')
+                    let downloadButtonInterval = setInterval(() => {
+                        if (downloadButtonCounter < allDownloadButtons.length) {
+                            allDownloadButtons[downloadButtonCounter].click();
+                            downloadButtonCounter++;
+                        } else {
+                            console.log("All Downloading Done")
+                            clearInterval(downloadButtonInterval);
+                        }
+                    }, 5000)
+                },
+            });
+
+            chrome.windows.update(downloadTab.windowId, { focused: true }, (window) => {
+                chrome.tabs.update(downloadTab.id, { active: true })
+            })
+        }, 6000)
     }
-}, 5000)
+}, 15000)
+
+
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.greeting === "GetURL") {
+        var tabURL = "Not set yet";
+        chrome.tabs.query({ active: true }, function (tabs) {
+            if (tabs.length === 0) {
+                sendResponse({});
+                return;
+            }
+            tabURL = tabs[0].url;
+            sendResponse({ navURL: tabURL });
+        });
+    }
+});
+
--- popup.js
+++ popup.js
@@ -1,5 +1,11 @@
 
 $(document).ready(() => {
-   
+    $('#start-automation-btn').click(() => {
+        chrome.runtime.sendMessage({ greeting: "GetURL" },
+            function (response) {
+                tabURL = response.navURL;
+                console.log(tabURL);
+            });
+    });
 })
 
